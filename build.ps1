67492daa790d2d6dcd6da143937bde8f394e3912
xhang1108 <xuehang_98@hotmail.com>
2026-04-10 10:45:40 +0800

    fix: varible naming

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $csc = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
-$dlls = Get-ChildItem -Recurse *.dll | Where { $_.Name -ne "BlenderCameraControls.dll" } | Group-Object Name | ForEach-Object { $_.Group[0].FullName }
-$args = @("/t:library", "/out:BlenderCameraControls.dll")
-foreach ($dll in $dlls) { $args += "/r:$dll" }
-$args += "BlenderCameraControls.cs"
-& $csc $args
+$dlls = Get-ChildItem -Recurse *.dll | Where-Object { $_.Name -ne "BlenderCameraControls.dll" } | Group-Object Name | ForEach-Object { $_.Group[0].FullName }
+$cscParams = @("/t:library", "/out:BlenderCameraControls.dll")
+foreach ($dll in $dlls) { $cscParams += "/r:$dll" }
+$cscParams += "BlenderCameraControls.cs"
+& $csc $cscParams
